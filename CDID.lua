2154e463a3af7315d61d24a3ec3b69c828735670
EnzoAK <79358946+TheAlmightyChipZ@users.noreply.github.com>
2022-11-16 12:49:37 +0800

    Update CDID.lua

--- CDID.lua
+++ CDID.lua
@@ -51,6 +51,28 @@ local Slider = MainTab:CreateSlider({
 	end,
 })
 local Button = MainTab:CreateButton({
+	Name = "Infinite Jump",
+	Callback = function()
+        local g = getgenv()
+
+if g._swim then
+    g._swim:Disconnect()
+    g._swim = nil
+end
+
+local u = game:GetService("UserInputService")
+g._swim = u.InputBegan:Connect(function(input, gameProcessedEvent)
+    if gameProcessedEvent then
+        return
+    end
+
+    if input.KeyCode == Enum.KeyCode.Space then
+        game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+    end
+end)
+	end,
+})
+local Button = MainTab:CreateButton({
 	Name = "Rejoin Server [Doesn't work in VIP]",
 	Callback = function()
         game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
@@ -156,7 +178,7 @@ local Toggle = ESPTab:CreateToggle({
 
 local TeleportTab = Window:CreateTab("Teleport", 4483362458)
 
-local Paragraph = TeleportTab:CreateParagraph({Title = "Player Teleport", Content = "[1]"})
+local Paragraph = TeleportTab:CreateParagraph({Title = "Player Teleport", Content = "[1] [Under Maintenance]"})
 
 local plrs = {}
     local selectedPlr
