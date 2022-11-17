f4e77e1a464dd37bda7932259179bb1c76957fd5
EnzoAK <79358946+TheAlmightyChipZ@users.noreply.github.com>
2022-11-17 11:58:31 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,7 +7,7 @@ if game.PlaceId == 8726743209 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheAlmightyChipZ/EnzoWareV1.1/main/EnzoWareV1.1%20Refinery%20Caves.lua")))()
 -- Car Driving Indonesia
 elseif game.PlaceId == 6911148748 then 
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheAlmightyChipZ/Private/main/CDID.lua")))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheAlmightyChipZ/Private/main/CDID.lua", true))()
 -- Universal Hub
 else
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheAlmightyChipZ/EnzoWareV1.1/main/EnzoWareV1.1%20Universal%20Hub.lua")))()
