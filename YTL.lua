64033b75077cb5fd344ecca979f1877b497439e1
EnzoAK <79358946+TheAlmightyChipZ@users.noreply.github.com>
2022-11-19 12:44:39 +0800

    Update YTL.lua

--- YTL.lua
+++ YTL.lua
@@ -1,1 +1,7 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
+Notification:Notify(
+    {Title = "Notice", Description = "Script not ready!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 15, Type = "default"}
+)
