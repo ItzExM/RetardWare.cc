b6ed74681d2cdb246111b8a61248bba4f0311f94
EnzoAK <79358946+TheAlmightyChipZ@users.noreply.github.com>
2022-11-20 20:31:10 +0800

    Delete YTL.lua

--- YTL.lua
+++
@@ -1,7 +0,0 @@
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-
-Notification:Notify(
-    {Title = "Notice", Description = "Script not ready!"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "default"}
-)
